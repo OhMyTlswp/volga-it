1337328b038c19950c28ac611a98e422579c57b1
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2020-03-22 00:51:42 +0300

    upd

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var burgerButton = document.querySelector('.header_button.burger_button');
+var burgerButton = document.querySelector('.header_button.burger_button'),
+  burgerMenu = document.querySelector('.burger_menu');
 burgerButton.addEventListener('click', function() {
   console.log(1)
   burgerButton.classList.toggle('opened');
+  burgerMenu.classList.toggle('closed');
 })
