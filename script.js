44a8dc22429a6fe0de18640be1200ee28454ec84
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2020-03-23 23:18:48 +0300

    add touch events

--- script.js
+++ script.js
@@ -54,6 +54,54 @@ window.onload = () => {
         }
       }
     })
+    var touchX = 0;
+    window.addEventListener('touchstart', function() {
+      touchX = event.touches[0].screenX
+    })
+    window.addEventListener('touchmove', function() {
+      if (!stopped) {
+        document.querySelector('.pages').style.transition = '2s';
+        stopped = true;
+        var stoppedTimeout = setTimeout(() => {
+          stopped = false;
+          document.querySelector('.pages').style.transition = '0s';
+        }, 2000);
+        if (event.touches[0].screenX - touchX < 0 && selectedPage < 4) {
+          selectedPage += 1;
+        }
+        if (event.touches[0].screenX - touchX > 0 && selectedPage > 0) {
+          selectedPage -= 1;
+        }
+        var x = 100 * selectedPage;
+        document.querySelector('.pages').style.transform = 'translateX(-' + x + 'vw)';
+        if (selectedPage >= 1) {
+          document.querySelector('.logo').classList.add('green');
+        }
+        if (selectedPage === 1) {
+          document.querySelector('.page_2 .leaves').classList.add('view');
+        }
+        if (selectedPage === 2) {
+          document.querySelector('.page_3 .leaves').classList.add('view');
+          document.querySelector('.greenfield_autumn').classList.add('greenfield_autumn_animation');
+        }
+        if (selectedPage === 4) {
+          document.querySelector('.page_5 .leaves').classList.add('view');
+        }
+        if (selectedPage !== 4) {
+          document.querySelector('.page_5 .leaves').classList.remove('view');
+        }
+        if (selectedPage !== 2) {
+          document.querySelector('.page_3 .leaves').classList.remove('view');
+          document.querySelector('.greenfield_autumn').classList.remove('greenfield_autumn_animation');
+        }
+        if (selectedPage !== 1) {
+          document.querySelector('.page_2 .leaves').classList.remove('view');
+        }
+        if (selectedPage < 1) {
+          document.querySelector('.logo').classList.remove('green');
+        }
+      }
+    })
     window.addEventListener('mousemove', function() {
       if (selectedPage === 0) {
         document.querySelector('.fog').style.transform = 'translate(-' + event.pageX / 1000 + 'px,-' + event.pageY / 1000 + 'px)'
